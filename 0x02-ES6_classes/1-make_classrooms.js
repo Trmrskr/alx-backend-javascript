2df7aebac061cc8660e29748f227f4eeb3eb5917
Trmrskr <simonalale@gmail.com>
2023-06-29 13:32:25 +0000

    Return an array of classroom object

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from './0-classroom';
+
+/**
+ * Create and return an array of classRoom objects
+ */
+
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}
