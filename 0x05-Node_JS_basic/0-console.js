67aaa4d377a0ffb9c1218ad55645b1b22a958e0c
Trmrskr <simonalale@gmail.com>
2023-08-21 15:08:01 +0000

    Execiting basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Logs a message to the consoel
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+
+const displayMessage = (str_arg) => {
+  console.log(str_arg);
+}
+
+module.exports = displayMessage;
