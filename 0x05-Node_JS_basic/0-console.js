75db70dfde4542f14692c2b7f8d9408d397db13d
Trmrskr <simonalale@gmail.com>
2023-08-22 05:38:18 +0000

    build the console.log function

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -3,9 +3,5 @@
  * @param {string} message - The message to log.
  * @returns {void}
  */
-
-const displayMessage = (str_arg) => {
-  console.log(str_arg);
-}
-
+const displayMessage = (str_arg) => process.stdout.write(str_arg + '\n');
 module.exports = displayMessage;
