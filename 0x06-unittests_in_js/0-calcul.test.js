37abdefaf494a62d155f716a98a0d39cb0d0f432
Trmrskr <simonalale@gmail.com>
2023-08-25 14:40:09 +0000

    Basic test with mocha and Node assertion library: update

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,27 +2,11 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('floating point whole numbers', () => {
-    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
-  });
-
-  it('rounding down floating fractional number of b', () => {
-    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
-  });
-
-  it('rounding down floating point fractional number of a & b', () => {
-    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
-  });
-
-  it('rounding down floating point fractional number of a', () => {
-    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
-  });
-
-  it('rounding up floating point fractional number of a and b', () => {
-    assert.strictEqual(calculateNumber(1.5, 2.5), 5);
-  });
-
-  it('rounding down a & b floating point fractional numbers with trailing 9', () => {
-    assert.strictEqual(calculateNumber(2.499, 3.499), 5);
-  });
+	assert.equal(4, calculateNumber(1, 3))
+	assert.equal(5, calculateNumber(1.8, 3));
+	assert.equal(5, calculateNumber(1.8, 3.2));
+	assert.equal(4, calculateNumber(1, 3.2));
+	assert.equal(5, calculateNumber(1, 3.7));
+	assert.equal(5, calculateNumber(1.2, 3.7));
+	assert.equal(6, calculateNumber(1.5, 3.7));
 });
